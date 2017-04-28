0da79d9820eae17e82aa45f7f80d5783afea2f02
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-04-27 19:04:12 -0600

    Create Seed

---
+++ server/seed.js
@@ -0,0 +1,63 @@
+var path = require('path');
+var app = require(path.resolve(__dirname, './server'));
+var models = app.models;
+
+var accounts = [
+    {
+        name: 'Alex Fernandez',
+        username: 'afernandez',
+        email: 'olorenzo@outlook.com',
+        password: '123456'
+    },
+    {
+        name: 'Carlos Lopez',
+        username: 'clopez',
+        email: 'cyber_heroe@unitec.edu',
+        password: '123456'
+    }
+];
+
+//Users Seed
+var Account = app.models.Account;
+Account.create(accounts, function (err, records) {
+    if (err) 
+        throw err; //create the admin role
+    var Role = app.models.Role;
+    Role.create({
+        name: 'admin'
+    }, function (err, role) {
+        if (err) 
+            throw err; //make Alex an admin
+        var RoleMapping = app.models.RoleMapping;
+        role
+            .principals
+            .create({
+                principalType: RoleMapping.USER,
+                principalId: records[0].id
+            }, function (err, principal) {
+                if (err) 
+                    throw err;
+                }
+            );
+    });
+    console.log('Record created:', records);
+});
+
+
+
+//Example to create more seeds
+//Select the code below and hold ctrl press 'k' and then press 'c'
+// var phoneTypes = [
+//     {
+//         description: 'home'
+//     }, {
+//         description: 'mobile'
+//     }
+// ];
+// //phoneTypes Seed
+// var PhoneType = app.models.PhoneType;
+// PhoneType.create(phoneTypes, function (err, records) {
+//     if (err) 
+//         throw err;
+//     console.log("phoneTypes created!")
+// }); //genders Seed
