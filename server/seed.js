0ac2d6dbb0d69b300b974d8a324fd9ccf7f1be85
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-06-02 11:35:01 -0600

    update seed to add created and modified fields

--- server/seed.js
+++ server/seed.js
@@ -7,13 +7,17 @@ var accounts = [
         name: 'Alex Fernandez',
         username: 'afernandez',
         email: 'olorenzo@outlook.com',
-        password: '123456'
+        password: '123456',
+        createdAt: new Date(),
+        modifiedAt: new Date()
     },
     {
         name: 'Carlos Lopez',
         username: 'clopez',
         email: 'cyber_heroe@unitec.edu',
-        password: '123456'
+        password: '123456',
+        createdAt: new Date(),
+        modifiedAt: new Date()
     }
 ];
 
