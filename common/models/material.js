bf2903c62b4ddf94e83fd6aaace1d15ba16d0d36
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-06-02 11:33:59 -0600

    add material code and entityId to new materials

--- common/models/material.js
+++ common/models/material.js
@@ -1,5 +1,36 @@
 'use strict';
 
 module.exports = function(Material) {
+    var app = require('../../server/server');
+    Material.observe('before save', function updateTimestamp(ctx, next) {
+        if(ctx.isNewInstance){
+            ctx.instance.code = 1;
 
+            const User = app.models.Account;    // works!
+            const token = ctx.options && ctx.options.accessToken;
+            const userId = token && token.userId;
+
+            User.findById(userId,function(err,user){
+                console.log("user",user);
+                Material.find({where: {entityId: 1},order: 'code DESC', limit: 1,fields: {code: true}}, function(err,materials){
+                    console.log(materials);
+                    if(materials.length){
+                        if(ctx.instance){
+                            ctx.instance.entityId = user.entityId;
+                            ctx.instance.code = materials[0].code + 1;
+                        }else{
+                            ctx.data.entityId = user.entityId;
+                            ctx.data.code = materials[0].code + 1;
+                        }
+                    }
+                    next();
+                });    
+            });
+            
+            
+
+        }else{
+            next();
+        }
+    });
 };
