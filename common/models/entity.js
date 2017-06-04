351e7cf8025ca8bd735dbcd90e8039002b1fc2ff
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-06-04 11:52:49 -0600

    create user account when a entity is registered

--- common/models/entity.js
+++ common/models/entity.js
@@ -1,5 +1,38 @@
 'use strict';
 
 module.exports = function(Entity) {
+    var app = require('../../server/server');
 
+    Entity.afterRemote('create', function(ctx, entity, next) {
+        console.log("entity",entity)
+            const User = app.models.Account;    // works!
+            var newUser = {
+                name: entity.name,
+                isEnabled: true,            
+                username: entity.username,
+                password: entity.password,
+                email: entity.email,
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                entityId: entity.id
+            };
+            if(ctx.isNewInstance){
+                if(ctx.instance){
+                    ctx.instance.unsetAttribute('password');
+                }else{
+                    delete ctx.data.password;
+                }
+            }
+            entity.unsetAttribute("password");
+           // delete entity.password;
+            User.create(newUser,function(err,user){
+                console.log("new user",user);
+                if (err) {
+                    console.log(err);
+                    Entity.deleteById(entity.id);
+                    return next(err);
+                }
+                return next();
+            });
+    });
 };
