b9bf25893f621878975b8fbb27bffae86f58439d
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-06-05 15:07:57 -0600

    Code Generation (#9)
    * add code generation to Manpower and ToolsAndEquipment

--- common/models/manpower.js
+++ common/models/manpower.js
@@ -1,5 +1,33 @@
 'use strict';
 
 module.exports = function(Manpower) {
+     var app = require('../../server/server');
+    Manpower.observe('before save', function updateTimestamp(ctx, next) {
+        var code = 1;
+        var id = 0;
+        if(ctx.isNewInstance){
+            if(ctx.instance){
+                id = ctx.instance.entityId;
+            }else{
+                id = ctx.data.entityId;
+            }
+            Manpower.find({where: { entityId: id },order: 'code DESC', limit: 1,fields: {code: true}}, function(err,items){
+                if(err){
+                    return next(err);
+                }
 
+                if(items.length){
+                    code = items[0].code + 1;
+                }
+                if(ctx.instance){
+                    ctx.instance.code = code;
+                }else{
+                    ctx.data.code = code;
+                }
+                return next();
+            }); 
+        }else{
+            next();
+        }        
+    });
 };
