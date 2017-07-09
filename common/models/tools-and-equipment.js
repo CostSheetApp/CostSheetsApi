6e70a884f27b0db4205defd4e21563876be3d9a1
Carlos Luis <cyber_heroe@unitec.edu>
2017-07-09 00:22:00 -0600

    Agregar Costos Iniciales
    Se agrego la funcionalidad de los costos iniciales en:
    - Mano de Obra
    - Herramientas y equipo

--- common/models/tools-and-equipment.js
+++ common/models/tools-and-equipment.js
@@ -30,4 +30,24 @@ module.exports = function(Toolsandequipment) {
             next();
         }        
     });
+	
+	Toolsandequipment.observe('after save',function(ctx, next) {
+        console.log(ctx.instance);
+
+         if(ctx.isNewInstance){
+            if(!(ctx.instance.cost && ctx.instance.cost>0) && !(ctx.instance.regionId && ctx.instance.regionId>0)) next();
+
+            app.models.ToolsAndEquipmentCostHistory.create({toolsAndEquipmentId: ctx.instance.id,cost: ctx.instance.cost, regionId: ctx.instance.regionId},function(err,result){
+                console.log("if something hppen, run this")
+                if(err){
+                    next(err);
+                }
+                    next();
+            });
+        }else{
+            next();
+        }  
+
+        
+    });
 };
