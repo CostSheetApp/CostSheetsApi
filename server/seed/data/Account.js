fdacc02aa55823b772ff318caf9199b55ffa31fd
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-08-09 00:40:42 -0600

    add seed data

---
+++ server/seed/data/Account.js
@@ -0,0 +1,18 @@
+export default accounts = [
+    {
+        name: 'Alex Fernandez',
+        username: 'afernandez',
+        email: 'olorenzo@outlook.com',
+        password: '123456',
+        createdAt: new Date(),
+        modifiedAt: new Date()
+    },
+    {
+        name: 'Carlos Lopez',
+        username: 'clopez',
+        email: 'cyber_heroe@unitec.edu',
+        password: '123456',
+        createdAt: new Date(),
+        modifiedAt: new Date()
+    }
+];
---
+++ server/seed/seed.js
@@ -0,0 +1,83 @@
+var path = require('path');
+//var app = require(path.resolve(__dirname, '../server'));
+
+
+
+//var models = app.models;
+
+
+module.exports = {
+    AddJobs: function(model,jobs) {
+        model.create(jobs, function(err, res) {
+            if (err) {
+                throw err;
+                //return false
+            }
+
+            return true;
+        });
+    },
+    AddRegions: function(model,regions) {
+        model.create(regions, function(err, res) {
+            if (err) {
+                throw err;
+                //return false
+            }
+            return true;
+        });
+    },
+    AddUnitsOfMeasurements: function(app) {
+        var UoMs = require('./data/UnitsOfMeasurement');
+        app.models.UnitsOfMeasurement.create(UoMs, function(err, res) {
+            if (err) {
+                return false
+            }
+
+            return true;
+        //console.log('Record created:', records);
+        });
+    }
+};
+
+// var CreateUnitsOfMeasurement = () => {
+//     var UnitsOfMeasurement = app.models.UnitsOfMeasurement;
+//     //UnitsOfMeasurement seed
+//     UnitsOfMeasurement.create(UoM, function (err, records) {
+//         if (err) 
+//             throw err;
+//         console.log("UnitsOfMeasurement created");
+//     });
+// }
+
+// var CreateEntity = (entity) => {
+//     //Accounts Seed
+//     var Entity = app.models.Entity;
+//     Entity.create(entity, function (err, res) {
+//         if (err) 
+//             throw err; //create the admin role
+//         // var Role = app.models.Role;
+//         // Role.create({
+//         //     name: 'admin'
+//         // }, function (err, role) {
+//         //     if (err) 
+//         //         throw err; //make Alex an admin
+//         //     var RoleMapping = app.models.RoleMapping;
+//         //     role
+//         //         .principals
+//         //         .create({
+//         //             principalType: RoleMapping.USER,
+//         //             principalId: records[0].id
+//         //         }, function (err, principal) {
+//         //             if (err) 
+//         //                 throw err;
+//         //             }
+//         //         );
+//         // });
+//         console.log('Record created:', records);
+//     });
+// }
+
+
+// var AddRegionsToEntity = (entity)=>{
+
+// }
