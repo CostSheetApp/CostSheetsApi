e9871d5e946cd7fe5050ddaedb72c84da1b5eefc
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-08-09 17:02:22 -0600

    mailgun

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -1,18 +1,18 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  Mail: {
-    transports: [
-      {
-        type: "SMTP",
-        secure: true,
-        host: process.env.MAILGUN_SMTP_SERVER,
-        port: process.env.MAILGUN_SMTP_PORT,
-        auth: {
-          user: process.env.MAILGUN_SMTP_LOGIN,
-          pass: process.env.MAILGUN_SMTP_PASSWORD
-        }
-      }
-    ]
-  }
-}
+// module.exports = {
+//   Mail: {
+//     transports: [
+//       {
+//         type: "SMTP",
+//         secure: true,
+//         host: process.env.MAILGUN_SMTP_SERVER,
+//         port: process.env.MAILGUN_SMTP_PORT,
+//         auth: {
+//           user: process.env.MAILGUN_SMTP_LOGIN,
+//           pass: process.env.MAILGUN_SMTP_PASSWORD
+//         }
+//       }
+//     ]
+//   }
+// }
