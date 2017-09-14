af4a74e8a4926a99e871fd39475c5554e625273b
Alex Omar Fernandez Lorenzo <olorenzo@outlook.com>
2017-09-14 08:45:57 -0600

    avoid email verification

--- common/models/account.js
+++ common/models/account.js
@@ -28,13 +28,14 @@ module.exports = function(Account) {
           protocol: protocol,
           text: text,
         };
-        account.verify(options, function(err, response) {
-          if (err) {
-            Account.deleteById(account.id);
-            return next(err);
-          }
-          next();
-        });
+        // account.verify(options, function(err, response) {
+        //   if (err) {
+        //     Account.deleteById(account.id);
+        //     return next(err);
+        //   }
+        //   next();
+        // });
+        next();
       } else {
         next();
       }
